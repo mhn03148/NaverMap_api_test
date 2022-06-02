9c867a12d386bd34ef73520570c9187cb6630cbb
jujinhyeong <mhn03148@naver.com>
2022-06-02 17:26:02 +0900

    naverMap api prectice

---
+++ app/src/main/java/com/example/navermap_api_test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.navermap_api_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//import com.naver.maps.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
